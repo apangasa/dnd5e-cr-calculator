aea9ebdda57a4ed78c882c854bd7cd52669d9b68
apangasa <60531810+apangasa@users.noreply.github.com>
2022-01-09 12:36:11 -0500

    Add class for Ability Scores and enumeration for damage types

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 class ClassStatSummary:
     def __init__(self, prof, ac, hp_min, hp_max, attack_bonus, dpr_min, dpr_max, save_dc):
         self.proficiency_bonus = prof
@@ -25,4 +27,38 @@ class IndividualStatSummary:
     def is_offensive_member_of(self, class_stat_summary):
         # TODO check if monster relies more on attack bonus or save DC
         return self.dpr in class_stat_summary.dpr and abs(self.attack_bonus - class_stat_summary.attack_bonus) < 2:
+
+class Monster:
+    def __init__(self, name, size, type, alignment, ability_scores, ac, hp, damage_multipliers, exp_cr):
+        pass
+
+
+class AbilityScores:
+    def __init__(self, str_, dex, con, int_, wis, cha):
+        self.strength = str_
+        self.dexterity = dex
+        self.constitution = con
+        self.intelligence = int_
+        self.wisdom = wis
+        self.charisma = cha
+
+class DamageMultipliers:
+    def __init__(self, multipliers):
+        self.multipliers = multipliers
+
+class DamageType(Enum):
+    ACID = 1
+    BLUDGEONING = 2
+    COLD = 3
+    FIRE = 4
+    FORCE = 5
+    LIGHTNING = 6
+    NECROTIC = 7
+    PIERCING = 8
+    POISON = 9
+    PSYCHIC = 10
+    RADIANT = 11
+    SLASHING = 12
+    THUNDER = 13
+
 
